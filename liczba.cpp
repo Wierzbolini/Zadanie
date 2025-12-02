d36b7d0c8329ab56b1a04eb6e23d001463f38239
Wierzbolini <m-zaklikiewicz@o2.pl>
2025-12-02 21:26:52 +0100

    Dodano sprawdzenie czy liba jest ujemna

--- liczba.cpp
+++ liczba.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 using namespace std;
 
+void czy_ujemna(int liczba){
+    if(liczba <= 0){
+        cout << "Podana liczba nie jest naturalna, koncze dzialanie...";
+        abort();
+    }
+}
+
 int main(){
     int n;
     cin >> n;
+    czy_ujemna(n);
     cout << n << endl;
     return 0;
 }
